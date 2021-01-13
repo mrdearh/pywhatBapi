45c424b88e281cd41aa346a2ac26f96dca3f1783
Sai Jeevan Puchakayala <saijeevan2002@gmail.com>
2021-01-13 07:51:07 +0530

    Add files via upload

---
+++ pywhatBapi/__init__.py
@@ -0,0 +1,29 @@
+import pyautogui as pg
+import webbrowser as web
+import time
+import pandas as pd
+
+def sendwhatmsgs(csv_file):
+    Messages = pd.read_csv(csv_file)
+    Messages_dict = Messages.to_dict('list')
+    Mobile_Numbers = Messages_dict['Mobile_Number']
+    messages = Messages_dict['Message']
+    Number_Message = zip(Mobile_Numbers,messages)
+    first = True
+    for Mobile_Numbers,message in Number_Message:
+        time.sleep(4)
+        web.open("https://api.whatsapp.com/send?phone="+Mobile_Numbers+"&text="+message)
+        if first:
+            time.sleep(10)
+            first=True
+        width,height = pg.size()
+        pg.click(width/2,height/2)
+        time.sleep(10)
+        pg.press('enter')
+        time.sleep(10)
+        pg.hotkey('alt','f4') 
+        pg.hotkey('ctrl','w')
+    return ("Messages sent Succesfully.")
+
+csv_file = "Whatsapp_Messages.csv"
+sendwhatmsgs(csv_file)
